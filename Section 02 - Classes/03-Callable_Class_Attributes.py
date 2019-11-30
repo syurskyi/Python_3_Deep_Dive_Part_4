6f8b569d751bd14d0bcef18cca767c83189380d2
Grey <sergejyurskyj@yahoo.com>
2019-11-29 21:27:49 -0800

    Commited 03-Callable_Class_Attributes

--- Section 02 - Classes/03-Callable_Class_Attributes.py
+++ Section 02 - Classes/03-Callable_Class_Attributes.py
@@ -16,35 +16,34 @@ class Program:
         print(f'Hello from {Program.language}!')
 
 # %%
-Program.__dict__
-
-# %%
-'''
-As we can see, the `say_hello` symbol is in the class dictionary.
-
-We can also retrieve it using either `getattr` or dotted notation:
-'''
-
-# %%
-Program.say_hello, getattr(Program, 'say_hello')
-
-# %%
-'''
-And of course we can call it, since it is a callable:
-'''
-
-# %%
+print(Program.__dict__)
+#
+# # %%
+# '''
+# As we can see, the `say_hello` symbol is in the class dictionary.
+# We can also retrieve it using either `getattr` or dotted notation:
+# '''
+#
+# # %%
+print(Program.say_hello, getattr(Program, 'say_hello'))
+#
+# # %%
+# '''
+# And of course we can call it, since it is a callable:
+# '''
+#
+# # %%
 Program.say_hello()
-
-# %%
-getattr(Program, 'say_hello')()
-
-# %%
-'''
-We can even access it via the namespace dictionary as well:
-'''
-
-# %%
-Program.__dict__['say_hello']()
-
-# %%
+#
+# # %%
+# getattr(Program, 'say_hello')()
+#
+# # %%
+# '''
+# We can even access it via the namespace dictionary as well:
+# '''
+#
+# # %%
+print(Program.__dict__['say_hello'])
+#
+# # %%
