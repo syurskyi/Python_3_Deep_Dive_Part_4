6f87e52a005e2a9dd672a2d91cd5085c42729211
Grey <sergejyurskyj@yahoo.com>
2019-12-04 21:32:42 -0800

    Committed 56. Booleans - Coding

--- Section 4 Polymorphism and Special Methods/56. Booleans - Coding.py
+++ Section 4 Polymorphism and Special Methods/56. Booleans - Coding.py
@@ -5,7 +5,8 @@
 
 # %%
 '''
-As we know every object in Python has an associated truth value. Empty container types are falsy, non-zero numbers are truthy, zero numbers are falsy, etc.
+As we know every object in Python has an associated truth value. Empty container types are falsy, non-zero numbers 
+are truthy, zero numbers are falsy, etc.
 '''
 
 # %%
@@ -62,12 +63,14 @@ bool(l2)
 
 # %%
 '''
-So when we create custom iterables, as long as we have a `__len__` method implemented, we can actually skip implementing the `__bool__` method, and our class will remain consistent with other collection types behaviors (empty collections are falsy, otherwise truthy).
+So when we create custom iterables, as long as we have a `__len__` method implemented, we can actually skip implementing
+the `__bool__` method, and our class will remain consistent with other collection types behaviors (empty collections are falsy, otherwise truthy).
 '''
 
 # %%
 '''
-Let's implement the `__bool__` method though, just to see that if it is present it will get called instead of the `__len__` method:
+Let's implement the `__bool__` method though, just to see that if it is present it will get called instead of 
+the `__len__` method:
 '''
 
 # %%
@@ -95,9 +98,9 @@ bool(p2)
 
 # %%
 '''
-For classes that do not define `__len__` we may want to use the `__bool__` method. For example, consider a 2D `Point` class where we want to consider the origin point `(0,0)` falsy, and everything else truthy.
-
-By default, all instances of our `Point` class will be truthy (they have neither a `__len__` nor a `__bool__` method):
+For classes that do not define `__len__` we may want to use the `__bool__` method. For example, consider a 2D `Point`
+class where we want to consider the origin point `(0,0)` falsy, and everything else truthy.
+Bydefault, all instances of our `Point` class will be truthy (they have neither a `__len__` nor a `__bool__` method):
 '''
 
 # %%
@@ -151,7 +154,6 @@ bool(p2.x or p2.y)
 # %%
 '''
 This works because any `0` number is falsy.
-
 So we might think we can use this approach instead of the explicit `!= 0` comparisons:
 '''
 
@@ -189,7 +191,8 @@ except TypeError as ex:
 
 # %%
 '''
-we can see that we have an exception. Although we can work with truth values in most circumstances, Python insists that `__bool__` should return an actual boolean type.
+we can see that we have an exception. Although we can work with truth values in most circumstances, Python insists 
+that `__bool__` should return an actual boolean type.
 '''
 
 # %%
