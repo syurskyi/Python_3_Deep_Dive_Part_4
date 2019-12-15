0f13751b315a821e847d36a435f0cb0657b5bc51
Grey <sergejyurskyj@yahoo.com>
2019-12-14 16:27:29 -0800

    Committed 02-Class_Attributes update

--- Section 02 - Classes/02-Class_Attributes.py
+++ Section 02 - Classes/02-Class_Attributes.py
@@ -1,50 +1,35 @@
 # %%
 '''
 ### Class Attributes
-'''
-
-# %%
-'''
 As we saw, when we create a class Python automatically builds-in properties and behaviors into our class object, 
 like making it callable, and properties like `__name__`.
 '''
 
-# %%
 class Person:
     pass
 
-# %%
 print(Person.__name__)
 
-# %%
 '''
 `__name__` is a **class attribute**. We can add our own class attributes easily this way:
 '''
 
-# %%
 class Program:
     language = 'Python'
     version = '3.6'
 
-# %%
 print(Program.__name__)
-
-# %%
 print(Program.language)
-
-# %%
 print(Program.version)
 
 # %%
 '''
 Here we used "dotted notation" to access the class attributes. In fact we can also use dotted notation to set
- the class attribute:
+the class attribute:
 '''
 
 # %%
 Program.version = '3.7'
-
-# %%
 print(Program.version)
 
 # %%
@@ -54,11 +39,7 @@ But we can also use the functions `getattr` and `setattr` to read and write thes
 
 # %%
 print(getattr(Program, 'version'))
-
-# %%
 setattr(Program, 'version', '3.6')
-
-# %%
 print(Program.version, getattr(Program, 'version'))
 
 # %%
@@ -84,8 +65,6 @@ We could also just have used a `setattr` function call:
 
 # %%
 setattr(Program, 'y', 200)
-
-# %%
 print(Program.y, getattr(Program, 'y'))
 
 # %%
@@ -94,55 +73,34 @@ So where is the state stored? Usually in a dictionary that is attached to the **
 (often referred to as the **namespace** of the class):
 '''
 
-# %%
 print(Program.__dict__)
 
-# %%
 '''
 As you can see that dictionary contains our attributes: `language`, `version`, `x`, `y` with their corresponding 
 current values.
-'''
-
-# %%
-'''
 Notice also that `Program.__dict__` does not return a dictionary, but a `mappingproxy` object - this is essentially 
 a read-only dictionary that we cannot modify directly (but we can modify it by using `setattr`, or dotted notation).
-'''
-
-# %%
-'''
 For example, if we change the value of an attribute:
 '''
 
-# %%
 setattr(Program, 'x', -10)
 
-# %%
 '''
 We'll see this reflected in the underlying dictionary:
 '''
 
-# %%
 print(Program.__dict__)
 
 # %%
 '''
 #### Deleting Attributes
-'''
-
-# %%
-'''
 So, we can create and mutate class attributes at run-time. Can we delete attributes too?
 The answer of course is yes. We can either use the `del` keyword, or the `delattr` function:
 '''
 
 # %%
 del Program.x
-
-# %%
 print(Program.__dict__)
-
-# %%
 delattr(Program, 'y')
 
 # %%
@@ -161,11 +119,8 @@ Although `__dict__` returns a `mappingproxy` object, it still is a hash map and 
 
 # %%
 print(Program.__dict__['language'])
-
-# %%
 print(list(Program.__dict__.items()))
 
-# %%
 '''
 One word of caution: not every attribute that a class has lives in that dictionary (we'll come back to this later).
 For example, you'll notice that the `__name__` attribute is not there:
@@ -173,6 +128,4 @@ For example, you'll notice that the `__name__` attribute is not there:
 
 # %%
 print(Program.__name__)
-
-# %%
 print(__name__ in Program.__dict__)
